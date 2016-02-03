266235cde16778fb577e99ee22d6592cd7efaf33
Shawn Hooper <shawn@actionablebooks.com>
2016-02-02 23:15:58 -0500

    add form and submit buttons

--- editor.php
+++ editor.php
@@ -44,48 +44,57 @@ try {
 				<h2><?php echo $locale; ?></h2>
 			</header>
 			<main>
+				<form method="post">
 
-				<?php $i=0; foreach ($missing as $string) { ?>
+					<div class="submitbar">
+						<button class="submitbutton">Save Changes</button>
+					</div>
 
-					<table class="translate_item" id="translate-table-<?php echo $i; ?>">
-						<thead>
+					<?php $i=0; foreach ($missing as $string) { ?>
+
+						<table class="translate_item" id="translate-table-<?php echo $i; ?>">
+							<thead>
+								<tr>
+									<td><span class="key"></strong><?php echo $string->id; ?></span></td>
+									<td></td>
+									<td>Translated Text for <?php echo $locale; ?> locale</td>
+								</tr>
+							</thead>
+							<tbody>
 							<tr>
-								<td><span class="key"></strong><?php echo $string->id; ?></span></td>
-								<td></td>
-								<td>Translated Text for <?php echo $locale; ?> locale</td>
-							</tr>
-						</thead>
-						<tbody>
-						<tr>
-							<td class="original">
-								<p><span id="original-<?php echo $i;?>"><?php echo $string->defaultMessage; ?></span></p>
-							</td>
-							<td>
-								<button class="copybutton" data-id="<?php echo $i; ?>">Copy &gt;</button>
-							</td>
-							<td>
-								<textarea class="translation" name="<?php echo $string->id; ?>" id="translation-<?php echo $i; ?>"></textarea>
-							</td>
-						</tr>
-						</tbody>
-						<?php if ($string->description !== null && $string->description !== $string->defaultMessage) { ?>
-						<tfoot>
-							<tr>
-								<td colspan="3">
-									<strong>Context:</strong><br /><?php echo $string->description; ?>
+								<td class="original">
+									<p><span id="original-<?php echo $i;?>"><?php echo $string->defaultMessage; ?></span></p>
+								</td>
+								<td>
+									<button class="copybutton" data-id="<?php echo $i; ?>">Copy &gt;</button>
+								</td>
+								<td>
+									<textarea class="translation" name="<?php echo $string->id; ?>" id="translation-<?php echo $i; ?>"></textarea>
 								</td>
 							</tr>
-						</tfoot>
-						<?php } ?>
-					</table>
+							</tbody>
+							<?php if ($string->description !== null && $string->description !== $string->defaultMessage) { ?>
+							<tfoot>
+								<tr>
+									<td colspan="3">
+										<strong>Context:</strong><br /><?php echo $string->description; ?>
+									</td>
+								</tr>
+							</tfoot>
+							<?php } ?>
+						</table>
 
 
 
-				<?php
-					$i++;
-				}
-				?>
+					<?php
+						$i++;
+					}
+					?>
 
+					<div class="submitbar">
+						<button class="submitbutton">Save Changes</button>
+					</div>
+				</form>
 			</main>
 			<footer>
 				<hr />
